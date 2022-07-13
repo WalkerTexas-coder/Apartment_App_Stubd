8e08cdbcf55d33887840a6adc49f1167e7a1c454
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-07-13 15:45:48 -0700

    font awesome, scaffold --api

---
+++ db/migrate/20220713215551_create_apartments.rb
@@ -0,0 +1,19 @@
+class CreateApartments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :apartments do |t|
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :manager
+      t.string :email
+      t.string :price
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.string :pets
+      t.text :image
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
